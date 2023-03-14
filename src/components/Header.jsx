af1c7805743623951ba5e87ed356f5c4326e0091
Matsuo <121542364+MatsuoNagisa@users.noreply.github.com>
2023-03-14 11:22:10 +0900

    1st commit ヘッダー

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from "./Header.module.css"
+
+export const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <nav className={styles.gnav}>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/mylist">My List</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
